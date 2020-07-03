b8a7f310e06100decc1b794bc17f0f8ca317888e
TiagoNandes <tiagonandes@hotmail.com>
2020-07-03 18:39:34 +0100

    medication updates

---
+++ app/src/main/java/com/example/medlis/MedicationAdapterRecycler.java
@@ -0,0 +1,66 @@
+package com.example.medlis;
+
+//import android.support.annotation.NonNull;
+//import android.support.v7.widget.RecyclerView;
+//import android.util.Log;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.Button;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+//
+//import java.util.ArrayList;
+
+public class MedicationAdapterRecycler {
+//    public static class ViewHolder extends RecyclerView.ViewHolder {
+//        protected TextView txtTitle, txtBand;
+//        protected ImageView imgCover;
+//
+//        public ViewHolder(View itemView) {
+//            super(itemView);
+//            txtTitle = itemView.findViewById(R.id.txtTitle);
+//            txtBand = itemView.findViewById(R.id.txtBand);
+//            imgCover = itemView.findViewById(R.id.imgCover);
+//
+//            Button b = itemView.findViewById(R.id.button);
+//            b.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Log.d("mymusic", "Element " + getAdapterPosition() + " clicked.");
+//                }
+//            });
+//        }
+//
+//    }
+//
+//    private ArrayList<Album> myAlbumList;
+//
+//    public MyAdapterRecycler(ArrayList<Album> myAlbumList) {
+//        this.myAlbumList = myAlbumList;
+//    }
+//
+//
+//
+//    @NonNull
+//    @Override
+//    public MyAdapterRecycler.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+//        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.single_medication, viewGroup, false);
+//        return new ViewHolder(itemView);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull MyAdapterRecycler.ViewHolder viewHolder, int i) {
+//        Album item = myAlbumList.get(i);
+//        viewHolder.txtTitle.setText(item.getTitle());
+//        viewHolder.txtBand.setText(item.getBand());
+//        viewHolder.imgCover.setImageResource(item.getCover());
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return myAlbumList.size();
+//    }
+
+
+}
